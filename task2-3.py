e9fd774c7d38f1872670509bc19253dd44278744
preetavi <avinash.preetkaur@dal.ca>
2017-10-20 11:25:39 -0700

    copied from task2-2.py

---
+++ task2-3.py
@@ -0,0 +1,20 @@
+from weather import Weather
+weather = Weather()
+loc=input("enter the location ")
+location = weather.lookup_by_location(loc)
+condition = location.condition()
+a=[]
+# Get weather forecasts for the upcoming days.
+highestTemp=0
+hottestDay=0
+i=0
+for forecasts in location.forecast():
+ if i<5:
+   b=[]    
+   b.append(forecasts['text'])
+   b.append(forecasts['date'])
+   b.append(forecasts['high'])
+   b.append(forecasts['low'])
+   a.append(b)
+   i+=1
+
